c5ecbc66847ba474570cce153737157b03952b97
nikhilbhardwaj52 <2906nikhilbhardwaj@gmail.com>
2025-06-10 01:24:53 +0530

    my first commit

---
+++ day1/react2.js
@@ -0,0 +1,7 @@
+
+const element  = React.createElement("h1",{id:"money", class:"Rohit" , style:{fontSize:"20px",backgroundColor:"blue",color:"red"}},"Hello Coder Army");
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(element);
